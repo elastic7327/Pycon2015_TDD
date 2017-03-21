fd0c4dcea9787c0af5b6fddfa5612f3d6318276a
elastic7327 <elastic7327@gamil.commit>
2017-03-21 17:52:59 +0800

    s

--- functional_testing.py
+++ functional_testing.py
@@ -26,13 +26,14 @@ class HomePageTest(unittest.TestCase):
         inputbox.send_keys("Buy peacock feathers")
         #
         #
+        import time
+        time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows)
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+                '1: Buy peacock feathers',
+                [row.text for row in rows]
         )
-        #
-        #
         #
         self.fail("Finish the test!!")
 
